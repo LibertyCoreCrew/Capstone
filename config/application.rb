6c4e95f43ffe46752beeb051ee4383b4a59404d9
Jason Stratman <jpstratman@unomaha.edu>
2018-02-07 20:24:30 -0600

    Created new thread and catch for any errors in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,27 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Liberty
+  # Starts the back-end process thread
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+
+    if defined?(Rails::Server)
+      config.after_initialize do
+        Thread.new do
+          begin
+            # Do back end stuff here
+            puts 'Do back-end stuff'
+          rescue StandardError => e
+            puts "Error during processing: #{e}"
+            puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
+            puts e.class
+            raise e
+          end
+          # We will do back-end stuff here.
+        end
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
