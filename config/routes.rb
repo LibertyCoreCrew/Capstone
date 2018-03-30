44ece5b8403860ba18db8d35f78de7b828fc4c17
Jason Stratman <jpstratman@unomaha.edu>
2018-03-29 21:52:10 -0500

    Started dashboard page formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-	root'pages#login'
+  root'pages#login'
 
   get 'pages/login'
 
   get 'pages/index'
 
   get 'pages/admin'
-  
+
   get 'pages/dashboard'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
