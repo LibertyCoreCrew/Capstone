4195f9080f7173119ce73390c9a9833f0e7b396a
Jason Stratman <jpstratman@unomaha.edu>
2018-03-30 10:50:40 -0500

    Made login page prettier and fixed routes to not require /pages/ before page name.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  root'pages#login'
+  root 'pages#login'
 
-  get 'pages/login'
+  get '/login' => 'pages#login'
 
-  get 'pages/index'
+  get '/index' => 'pages#index'
 
-  get 'pages/admin'
+  get '/admin' => 'pages#admin'
 
-  get 'pages/dashboard'
+  get '/dashboard' => 'pages#dashboard'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
