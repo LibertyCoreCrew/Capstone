c93a2afd76514e25ec9318eded7195ab0b95c8c0
John <jtlawson@unomaha.edu>
2018-04-05 23:00:13 -0500

    Slightly functioning drive api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,20 @@
 class PagesController < ApplicationController
-	include DriveManager
-	before_action :authenticate_user!, except: [:login]
+	#include DriveManager
+    require 'google/apis/drive_v2'
+    require 'googleauth'
+    require 'googleauth/stores/file_token_store'
+
+    require 'fileutils'
+    OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+    APPLICATION_NAME = 'Drive API Ruby Quickstart'
+    CLIENT_SECRETS_PATH = Dir.pwd + '/app/controllers/client_secret.json'
+    CREDENTIALS_PATH = File.join(Dir.home, '.credentials', "drive-ruby-quickstart.yaml")
+    SCOPE = Google::Apis::DriveV2::AUTH_DRIVE_METADATA_READONLY
+    #puts Dir.pwd + '/client_secret.json'
+    #puts CLIENT_SECRETS_PATH
+	#before_action :authenticate_user!, except: [:login]
   def login
-      redirect_to new_user_session_path
+      #redirect_to new_user_session_path
   end
 
   def index
@@ -15,6 +27,44 @@ class PagesController < ApplicationController
   end
   
   def documents
-  @user_id = ENV["LC_PRIVATE_KEY_ID"]
-  end
+
+    ##
+    # Ensure valid credentials, either by restoring from the saved credentials
+    # files or intitiating an OAuth2 authorization. If authorization is required,
+    # the user's default browser will be launched to approve the request.
+    #
+    # @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+    def authorize
+     
+      FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+
+      client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
+      token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
+      authorizer = Google::Auth::UserAuthorizer.new(
+        client_id, SCOPE, token_store)
+      user_id = 'default'
+      credentials = authorizer.get_credentials(user_id)
+      if credentials.nil?
+        url = authorizer.get_authorization_url(
+          base_url: OOB_URI)
+        puts "Open the following URL in the browser and enter the " +
+             "resulting code after authorization"
+        puts url
+        code = gets
+        credentials = authorizer.get_and_store_credentials_from_code(
+          user_id: user_id, code: code, base_url: OOB_URI)
+      end
+      credentials
+    end
+    # Initialize the API
+    service = Google::Apis::DriveV2::DriveService.new
+   
+    service.client_options.application_name = APPLICATION_NAME
+    service.authorization = authorize
+    @service = service.list_files()
+    
+ end
+  
 end
+# b = PagesController.new
+# r = b.documents
--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 20180404014914) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
