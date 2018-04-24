3930662a15c61a2e80881b1a4bc2607ef026f989
Brock Hinkson <brockhinkson@gmail.com>
2018-04-24 04:42:26 +0000

    Added string processing functionality. Each file's project/tract numbers can be extracted.

--- config/drivemanager/drivenlp.rb
+++ config/drivemanager/drivenlp.rb
@@ -1,7 +1,45 @@
 module DriveManager
   # Class for performing natural language processing
   class DriveNLP
-    def self.process(file, contents)
+    def self.process( file, contents )
+        
+        unless contents.empty?
+            c = contents.split()
+            p, t = c.index( "OPW" ), c.index( "Tract" )
+            proj_num, tract_num = "", ""
+            proj_num_found, tract_num_found = false, false
+            
+            
+            if !p.nil? or !t.nil?
+                puts "File name: " + file.name
+            end
+            
+            # Finds the OPW Project number, also avoids files that don't contain identifying information
+            unless p.nil?
+              for i in p..c.length-1
+                  if c[i] !~ /\D/       # This finds the next character block that is all-numeric
+                      proj_num = c[i]
+                      proj_num_found = true
+                      puts "Project: OPW " + proj_num
+                      break
+                  end
+              end
+            end
+            
+            # Finds the Tract number, also avoids files that don't contain identifying information
+            unless t.nil?
+              for i in t..c.length-1
+                  if c[i] !~ /\D/       # This finds the next character block that is all-numeric
+                      tract_num = c[i]
+                      tract_num_found = true
+                      puts "Tract: No. " + tract_num
+                      break
+                  end
+              end
+            end
+            
+        end
+      
     end
   end
 end
