eca43f98368710def2226345db7f693ff47399c7
Jason Stratman <jpstratman@unomaha.edu>
2018-05-04 11:10:18 -0500

    Revert "Now correctly checks for admin table before attempting a drop"
    This reverts commit fd363d131ec599d6e6e1e7401d7a9efa60e3f96a.

--- db/migrate/20180416033442_drop_admins.rb
+++ db/migrate/20180416033442_drop_admins.rb
@@ -1,5 +1,5 @@
 class DropAdmins < ActiveRecord::Migration[5.1]
   def change
-    drop_table :admins if ActiveRecord::Base.connection.table_exists? 'admins'
+    drop_table :admins if ActiveRecord::Base.connection.table_exist 'admins'
   end
 end
